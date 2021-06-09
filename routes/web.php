c0046b020a059781a6a92d2eeaa2a6e425847163
3e7o <223eto@gmail.com>
2021-06-09 02:35:35 -0400

    ruta actualizada

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ use App\Http\Controllers\StatController;
 */
 
 Route::get('/', function () {
-    return view('auth\login');
+    return view('login');
 });
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
