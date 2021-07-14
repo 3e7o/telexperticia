87c9803ef83cc4358b3d068ae1d276c939f79a73
3e7o <223eto@gmail.com>
2021-07-14 18:20:49 -0400

    fix google meet

--- config/google-meet.php
+++ config/google-meet.php
@@ -1,7 +1,12 @@
 <?php
 
     return [
-        'https://meet.google.com/oqn-pkgg-urh',
-        'https://meet.google.com/hjf-uijb-owv',
-        'https://meet.google.com/hkn-uhmv-bxr',
+        'https://meet.google.com/ewr-exxv-rjx',
+        'https://meet.google.com/vbv-nizf-kau',
+        'https://meet.google.com/fdi-wqco-tah',
+        'https://meet.google.com/ymy-ocdb-fvh',
+        'https://meet.google.com/ngn-uvgm-eag',
+        'https://meet.google.com/gww-iapp-gfw',
+        'https://meet.google.com/uma-cdxr-mwi',
+        'https://meet.google.com/mob-dzza-fzr'
     ];
